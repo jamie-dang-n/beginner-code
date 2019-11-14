3516e8a25b00a0bfed01922ae1f5bbd8226ca791
gothhouse <jamiedang1349@gmail.com>
2019-11-14 08:56:07 -0800

    Finished PotionSpawn function

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using CreatorKitCode;
 
 public class SpawnerSample : MonoBehaviour
@@ -7,26 +7,19 @@ public class SpawnerSample : MonoBehaviour
 
     void Start()
     {
-        int angle = 15;
+
+        SpawnPotion(0);
+        SpawnPotion(45);
+        SpawnPotion(90);
+        SpawnPotion(135);
+    }
+
+    void SpawnPotion(int angle)
+    {
         int radius = 5;
         Vector3 spawnPosition = transform.position;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 135;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
